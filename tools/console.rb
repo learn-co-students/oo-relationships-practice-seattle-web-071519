e87cdec1394d2d6ec14be49bbbabcc35c2512284
marshallhelenm <marshallhelenm@gmail.com>
2019-07-22 14:22:44 -0700

    done

--- tools/console.rb
+++ tools/console.rb
@@ -8,7 +8,7 @@ end
 
 # Pry.start
 
-
+## seed data for IMDB Lab
 sharknado = Movie.new("Sharknado")
 prejudice = Movie.new("Pride & Prejudice")
 shrek = Movie.new("Shrek")
@@ -19,7 +19,19 @@ btvs = Show.new("Buffy the Vampire Slayer")
 gellar = Actor.new("Sarah Michelle Gellar")
 dinklage = Actor.new("Peter Dinklage")
 
-buffy = btvs.cast_character("Buffy Summers", "Sarah Michelle Gellar")
+
+##seed data for crowdfunding lab
+helen = User.new("Helen")
+rosie = User.new("Rosie")
+kas = User.new("Kaspar")
+
+dice = Project.new("Awesome Dice", helen, 500)
+plush = Project.new("The Softest Plush", rosie, 6000)
+
+kas_pledge_1 = Pledge.new(kas, dice, 25)
+kas_pledge_2 = Pledge.new(kas, plush, 50)
+rosie_pledge_1 = Pledge.new(rosie, plush, 500)
+helen_pledge_1 = Pledge.new(helen, plush, 250)
 
 
 binding.pry
